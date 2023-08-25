2913476c2cf66658faabbc4c99f665f6bbb14aab
sriworks12 <127284473+sriworks12@users.noreply.github.com>
2023-08-25 21:55:20 +0530

    All Backend Files
    BackEnd IMS

---
+++ firebase.js
@@ -0,0 +1,13 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyBXr4RC4jGUP0J3oOVCBaCyiIW_yDs0c38",
+    authDomain: "userauthenticationsystem-e792a.firebaseapp.com",
+    projectId: "userauthenticationsystem-e792a",
+    storageBucket: "userauthenticationsystem-e792a.appspot.com",
+    messagingSenderId: "776893339404",
+    appId: "1:776893339404:web:e07da60bcb474b6fad1b29",
+    measurementId: "G-X09FN7SMVD"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
